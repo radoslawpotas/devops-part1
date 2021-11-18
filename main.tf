f1addfeca11a47a4e80a974bfa83e2300e22de9b
radoslaw.potas <radoslaw.potas@gmail.com>
2021-11-19 00:46:18 +0100

    Update terraform backend and terraform process

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 0.14.9"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.65"
     }
   }
-
-  required_version = ">= 0.14.9"
+  backend "azurerm" {
+    resource_group_name  = "tf_main_storage"
+    storage_account_name = "tfmainstorageaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
